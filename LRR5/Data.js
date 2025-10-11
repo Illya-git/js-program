63e9794f2f5716de35186c2f968285355b8a5ce8
Illya-git <ibagrij88@gmail.com>
2025-10-11 11:34:20 +0300

    Create Data.js

---
+++ LRR5/Data.js
@@ -0,0 +1,44 @@
+const items = [
+  {
+    id: 1,
+    title: "Pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: "Delicious pancakes with maple syrup",
+  },
+  {
+    id: 2,
+    title: "Burger",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: "Juicy beef burger with cheese",
+  },
+  {
+    id: 3,
+    title: "Milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: "Refreshing vanilla milkshake",
+  },
+  {
+    id: 4,
+    title: "Tiramisu",
+    category: "dessert",
+    price: 9.99,
+    img: "./images/item-4.jpeg",
+    desc: "Italian dessert with mascarpone and coffee",
+  },
+  {
+    id: 5,
+    title: "Omelette",
+    category: "breakfast",
+    price: 10.99,
+    img: "./images/item-5.jpeg",
+    desc: "Fluffy omelette with vegetables",
+  },
+];
+
+export default items;
