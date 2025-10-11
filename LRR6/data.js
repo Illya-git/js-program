4db8acee102bbfb09077e1bcbf0c6c6e86c8414e
Illya-git <ibagrij88@gmail.com>
2025-10-11 11:42:41 +0300

    Create data.js

---
+++ LRR6/data.js
@@ -0,0 +1,25 @@
+const people = [
+  {
+    id: 1,
+    image: "./images/person-1.jpg",
+    name: "Олександр Піщур",
+    title: "Front-End Developer",
+    quote: "Люблю React і створювати сучасні UI!",
+  },
+  {
+    id: 2,
+    image: "./images/person-2.jpg",
+    name: "Ірина Іваненко",
+    title: "UX Designer",
+    quote: "Інтерфейси мають бути не лише красивими, а й зручними.",
+  },
+  {
+    id: 3,
+    image: "./images/person-3.jpg",
+    name: "Дмитро Заваров",
+    title: "Project Manager",
+    quote: "Командна робота — ключ до успіху будь-якого проєкту.",
+  },
+];
+
+export default people;
