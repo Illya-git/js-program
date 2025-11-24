bf93f1208896f2d1f3391c6c0d5ce41361e49252
Illya-git <ibagrij88@gmail.com>
2025-11-24 16:09:02 +0200

    Create reducer.js

---
+++ LR10/reducer.js
@@ -0,0 +1,58 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
+
+  if (action.type === "INCREASE") {
+    const tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+    const tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "GET_TOTALS") {
+    const { total, amount } = state.cart.reduce(
+      (acc, item) => {
+        acc.total += item.price * item.amount;
+        acc.amount += item.amount;
+        return acc;
+      },
+      { total: 0, amount: 0 }
+    );
+    return { ...state, total: parseFloat(total.toFixed(2)), amount };
+  }
+
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+
+  return state;
+};
+
+export default reducer;
