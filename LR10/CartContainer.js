52a63d7865590f6c16fcde458859648f24ca36c1
Illya-git <ibagrij88@gmail.com>
2025-11-24 16:10:30 +0200

    Create CartContainer.js

---
+++ LR10/CartContainer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import CartItem from "./CartItem";
+
+const CartContainer = () => {
+  const { cart, total, clearCart, loading } = useGlobalContext();
+
+  if (loading) {
+    return <h2 className="loading">Loading...</h2>;
+  }
+
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your Bag</h2>
+      </header>
+      <div>
+        {cart.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={clearCart}>
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
