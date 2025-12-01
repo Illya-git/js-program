7d8648067bedd1d4aef18af93c097e3d032c0c96
Illya-git <ibagrij88@gmail.com>
2025-12-01 11:17:11 +0200

    Create App.js

---
+++ LR13/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const user = useSelector((state) => state.user.user);
+
+  return (
+    <div className="App">
+      {user ? <Logout /> : <Login />}
+    </div>
+  );
+}
+
+export default App;
